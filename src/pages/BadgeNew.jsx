cc245d2ae750ae0b3c9411e7e91035a9917b311f
Mario Alejandro Cortez Salinas <mario_1605@live.com.mx>
2020-03-09 22:09:04 -0600

    feature/header od new badge

--- src/pages/BadgeNew.jsx
+++ src/pages/BadgeNew.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from '../components/Navbar.jsx'
+import header from '../images/badge-header.svg'
+import '../styles/BadgeNew.scss'
 
 function BadgeNew() {
     return(
         <>
             <Navbar />
+            <div className="BadgeNew__hero">
+                <img 
+                    className="img-fluid" 
+                    src={header} 
+                    alt="Logo"
+                />
+            </div>
         </>
     )
 }
