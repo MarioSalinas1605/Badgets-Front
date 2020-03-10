975471ec9fb2414c96d30f2a3ff36cd8070bfdd2
Mario Alejandro Cortez Salinas <mario_1605@live.com.mx>
2020-03-10 09:16:58 -0600

    refactor/adding badge to badgeNew page

--- src/pages/BadgeNew.jsx
+++ src/pages/BadgeNew.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
+import Badge from '../components/Badge.jsx'
 import Navbar from '../components/Navbar.jsx'
 import header from '../images/badge-header.svg'
 import '../styles/BadgeNew.scss'
 
 function BadgeNew() {
-    return(
+    return (
         <>
             <Navbar />
             <div className="BadgeNew__hero">
-                <img 
-                    className="img-fluid" 
-                    src={header} 
+                <img
+                    className="img-fluid"
+                    src={header}
                     alt="Logo"
                 />
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Badge
+                            firstName="Richard"
+                            lastName="Kaufman"
+                            twitter="sparragus"
+                            jobTitle="Frontend Engineer"
+                            avatarUrl="https://www.gravatar.com/avatar/21594ed15d68ace3965642162f8d2e84?d=identicon"
+                        />
+                    </div>
+                </div>
             </div>
         </>
     )
