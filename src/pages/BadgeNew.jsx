8e923bc1c753565f149d19208bd066f90647a2de
Mario Alejandro Cortez Salinas <mario_1605@live.com.mx>
2020-03-09 21:49:13 -0600

    feature/header

---
+++ src/pages/BadgeNew.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+
+function BadgeNew() {
+    return(
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default BadgeNew
